d76e86b94dea681fdb7b00328dbbb7a2661c5719
Vitalii <vitalylqw@gmail.com>
2021-12-02 02:42:20 +0300

    comit 3

--- program.py
+++ program.py
@@ -1,9 +1,3 @@
 print('Я домашка')
 print('Привет')
 
-from typing import Sequence
-def lower_join(seq: Sequence) -> str:
-    """Принимает на вход последовательность и создаёт из неё  
-    строку в нижнем регистре."""
-    return ''.join(seq).lower()
-lower_join('Привет')
---
+++ test.py
@@ -0,0 +1,10 @@
+from typing import Sequence
+
+
+def lower_join(seq: Sequence) -> str:
+    """Принимает на вход последовательность и создаёт из неё  
+    строку в нижнем регистре."""
+    return ''.join(seq).lower()
+
+
+print(lower_join(25))
